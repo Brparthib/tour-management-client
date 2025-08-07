828686ad5de18df30ee5dfd1f0dead5d441a6963
ParthibBr <bparthib77@gmail.com>
2025-08-07 10:31:23 +0600

    project initialized done

---
+++ src/components/layout/CommonLayout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react";
+
+interface IProps {children: ReactNode}
+
+export default function CommonLayout({children}: IProps) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
